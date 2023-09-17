f3d7c3c5e7a418b19238ad19b987ecb37f251676
Davide Garberi <dade.garberi@gmail.com>
2023-09-17 19:28:11 +0200

    powermodes-2 -> powermodes

--- powermodes/main.cpp
+++ powermodes/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "powermodes-2_" + QLocale(locale).name();
+        const QString baseName = "powermodes_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
