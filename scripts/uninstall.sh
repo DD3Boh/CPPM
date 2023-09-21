81c89a73d29b7ea3a085b92152fb0db622b9737c
Davide Garberi <dade.garberi@gmail.com>
2023-09-21 17:59:09 +0200

    scripts: uninstall: Rework
     * By deleting the mode file folder first, and then restarting
       the service, the mode automatically gets set to balanced,
       which is the best thing to do before uninstalling completely
     * Also automatically stop the services before disabling them
     * Change console debugging slightly as well.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -6,18 +6,20 @@
 #  SPDX-License-Identifier: LGPL-2.1-or-later
 #
 
-echo "Uninstalling CPPM..."
-echo " "
-
-sudo systemctl disable cppm
-
-cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B out/
-sudo cmake --build out/ --target uninstall
-
 echo "Removing system files..."
 if [ -d /opt/cppm ]; then
 	sudo rm -rf /opt/cppm
 fi
+
+echo "Resetting Power profile to balanced..."
+sudo systemctl start cppm.service
+
+echo "Uninstalling CPPM..."
+sudo systemctl stop cppm.service cppm.path
+sudo systemctl disable cppm
+
+cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B out/
+sudo cmake --build out/ --target uninstall
 
 echo " "
 echo "Uninstall finished!"
